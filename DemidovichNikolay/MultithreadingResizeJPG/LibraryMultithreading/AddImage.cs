5031b8212c6d8e5582c44205b4cbac48a2a06f9c
Nikolaj <36537488+MikalaiDzemidovich@users.noreply.github.com>
2020-05-11 18:45:39 +0300

    LOL
    +

---
+++ DemidovichNikolay/MultithreadingResizeJPG/LibraryMultithreading/AddImage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace LibraryMultithreading
+{
+    public class AddImage
+    {
+        public void PathImage(string path, out string[] image)
+        {
+            try
+            {
+                string[] addres = Directory.GetFiles(path);
+                image = addres;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+                image = default;
+            }
+        }
+    }
+}
