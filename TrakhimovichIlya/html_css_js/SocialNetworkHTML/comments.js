0618c15eaaf4e076a601e9b252d747e438d58cd4
Ilya-Trakhimovich <58522205+Ilya-Trakhimovich@users.noreply.github.com>
2020-06-10 19:01:32 +0300

    Social network "the cover"
    use html/css

---
+++ TrakhimovichIlya/html_css_js/SocialNetworkHTML/comments.js
@@ -0,0 +1,49 @@
+let comments = [];
+loadComments();
+
+document.getElementById('comment-add').onclick = function(){
+    let commentBody = document.getElementById('comment-body');
+
+    let comment = {
+        body : commentBody.value,
+        time : Math.floor(Date.now() / 1000)
+    }
+
+    commentBody.value = '';
+
+    comments.push(comment);
+    saveComments();
+    showComments();
+}
+
+function saveComments(){
+    localStorage.setItem('comments', JSON.stringify(comments));
+}
+
+function loadComments(){
+    if (localStorage.getItem('comments')) comments = JSON.parse(localStorage.getItem('comments'));
+    showComments();
+}
+
+function showComments (){
+    let commentField = document.getElementById('comment-field');
+    let out = '';
+    comments.forEach(function(item){
+        out += `<p class="text-right small"><em>${timeConverter(item.time)}</em></p>`;
+        out += `<p class="alert alert-success" role="alert">${item.body}</p>`;
+    });
+    commentField.innerHTML = out;
+}
+
+function timeConverter(UNIX_timestamp){
+    var a = new Date(UNIX_timestamp * 1000);
+    var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+    var year = a.getFullYear();
+    var month = months[a.getMonth()];
+    var date = a.getDate();
+    var hour = a.getHours();
+    var min = a.getMinutes();
+    var sec = a.getSeconds();
+    var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec ;
+    return time;
+  }
